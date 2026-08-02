89e50dafdb65424f89a47be49c700418e2e9acb3
Mark Kinyanjui <mckinyanjui3@gmail.com>
2026-08-03 02:46:29 +0300

    additional adjustments

---
+++ routes.py
@@ -0,0 +1,7 @@
+from server.controllers.auth_controller import auth_bp
+from server.controllers.task_controller import task_bp
+
+def register_routes(app):
+    """Register API blueprints using the paths expected by the JWT client."""
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(task_bp)
