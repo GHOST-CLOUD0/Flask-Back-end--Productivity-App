f1850bc2168a8fb5b0dd490d45cfdf7c7405ce3b
Mark Kinyanjui <mckinyanjui3@gmail.com>
2026-08-02 23:20:48 +0300

    created a class user

---
+++ server/models/user.py
@@ -0,0 +1,34 @@
+from server.extensions import db, bcrypt
+from server.models.base_model import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    tasks = db.relationship("Task", back_populates="user",cascade="all, delete-orphan",lazy=True)
+
+    @property
+    def password(self):
+        raise AttributeError("Password is write-only.")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash,password)
+
+    def to_dict(self):
+        user_dict = super().to_dict()
+        user_dict.update({
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+        })
+        return user_dict
+
+    def __repr__(self):
+        return f"<User {self.username}>"
