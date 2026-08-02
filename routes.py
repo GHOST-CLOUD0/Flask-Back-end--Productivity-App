ebc7a103c8e02cf64f9f9fc2f851483f41bc15b5
Mark Kinyanjui <mckinyanjui3@gmail.com>
2026-08-03 02:56:13 +0300

    added register routes function

--- routes.py
+++ routes.py
@@ -2,6 +2,8 @@ from server.controllers.auth_controller import auth_bp
 from server.controllers.task_controller import task_bp
 
 def register_routes(app):
-    """Register API blueprints using the paths expected by the JWT client."""
+    """
+    Register API blueprints using the paths expected by the JWT client.
+    """
     app.register_blueprint(auth_bp)
     app.register_blueprint(task_bp)
